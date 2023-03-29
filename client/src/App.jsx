4ab21bd9ba132966fb51aae1ce5a073353abb436
Abis <abisali@hotmail.com>
2023-03-29 22:19:33 +0400

    Setting up Dev Env (Vite)
    1) React set up using Vite.
    2) Default port changed to 3000.
    3) Clean up of irrelevant files.
    4) Clean up of irrelevant code.
    5) Node and Express set up.
    6) Backend is on port 5000.

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+const title = "Hello Abis and Fayyaz";
+
+let Heading = () => {
+  return (
+    <h1>{title}</h1>
+  )
+};
+
+function App() {
+  return (
+   <Heading/>
+  )
+};
+
+export default App
