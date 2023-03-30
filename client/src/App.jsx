42e7748e9e9f1ac665eafc0d1a6c51ade544ae08
Abis <abisali@hotmail.com>
2023-03-30 22:20:07 +0400

    Base grid layout setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,35 @@
-import './App.css'
+//Module Imports
+import { Container, Row, Col } from 'react-bootstrap';
 
-const title = "Hello Abis and Fayyaz";
+//Styling
+import './App.css';
 
-let Heading = () => {
-  return (
-    <h1>{title}</h1>
-  )
-};
+
 
 function App() {
   return (
-   <Heading/>
+   <Container fluid className='main-container'>
+    <Row className='row-one'>
+      <Col className='title-col'>
+        <h1 className='title'>HireHub</h1>
+      </Col>
+    </Row>
+    <Row className='row-two'>
+      <Col className='col-one' sm={1} md={4} lg={1}>
+        <div className='navigation'>
+          <button>News Feed</button>
+          <button>Upload CV icon</button>
+        </div>
+
+      </Col>
+      <Col className='col-two' sm={4} md={4} lg={4}>
+
+      </Col>
+      <Col className='col-three' sm={7} md={4} lg={7}>
+
+      </Col>
+    </Row>
+   </Container>
   )
 };
 
