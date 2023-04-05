7c2202897b83e1994ba8b4d194d778c684f7d6e8
Abis <abisali@hotmail.com>
2023-04-05 21:55:23 +0400

    Refactor JobList

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -7,7 +7,77 @@ import UploadModal from "./Cv";
 import "../App.css";
 
 let MainPage = () => {
+
+  // Bool for modal
   const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  //Date
+  var date = new Date();
+  var dateString = date.toLocaleDateString("en-GB");
+
+
+// get all job listings from the DB
+const [joblist, setJobList] = [
+  {
+      ID: 1,
+      Title: "Software Engineer",
+      Date: dateString,
+      Company: "ABC Inc.",
+      Location:"",
+      Position: "Full-time",
+      Description: "Developed and maintained software applications for clients",
+      Contact: "John Doe (john.doe@abcinc.com)",
+      Image:"",
+      },
+      
+      {
+      ID: 2,
+      Title: "Marketing Manager",
+      Date: dateString,
+      Company: "XYZ Corp.",
+      Location:"",
+      Position: "Contract",
+      Description: "Developed and executed marketing campaigns for new product launches",
+      Contact: "Jane Smith (jane.smith@xyzcorp.com)",
+      Image:"",
+      },
+      
+      {
+      ID: 3,
+      Title: "Sales Associate",
+      Date: dateString,
+      Company: "123 Enterprises",
+      Location:"",
+      Position: "Part-time",
+      Description: "Assisted customers with purchases and maintained store inventory",
+      Contact: "Mark Johnson (mark.johnson@123enterprises.com)",
+      Image:"",
+      },
+      
+      {
+      ID: 4,
+      Title: "Human Resources Manager",
+      Date: dateString,
+      Company: "Acme Corporation",
+      Location:"",
+      Position: "Full-time",
+      Description: "Managed employee relations, benefits, and performance evaluations",
+      Contact: "Sara Lee (sara.lee@acmecorp.com)",
+      Image:"",
+      },
+      
+      {
+      ID: 5,
+      Title: "Graphic Designer",
+      Date: dateString,
+      Company: "Design Co.",
+      Location:"",
+      Position: "Freelance",
+      Description: "Created designs for various marketing and advertising materials",
+      Contact: "Mike Brown (mike.brown@designco.com)",
+      Image:"",
+      },
+];
 
   const openModal = () => {
     setModalIsOpen(true);
--- client/src/components/JobList.jsx
+++ client/src/components/JobList.jsx
@@ -1,86 +1,24 @@
 //Module Imports
 import { Container, Row, Col } from "react-bootstrap";
-import React, { useState } from "react";
 
-let JobCard = () => {
 
-    var date = new Date();
-    var dateString = date.toLocaleDateString("en-GB");
-
-  const [joblist, setJobList] = [
-    {
-        ID: 1,
-        Title: "Software Engineer",
-        Date: dateString,
-        Company: "ABC Inc.",
-        Location:"",
-        Position: "Full-time",
-        Description: "Developed and maintained software applications for clients",
-        Contact: "John Doe (john.doe@abcinc.com)",
-        Image:"",
-        },
-        
-        {
-        ID: 2,
-        Title: "Marketing Manager",
-        Date: dateString,
-        Company: "XYZ Corp.",
-        Location:"",
-        Position: "Contract",
-        Description: "Developed and executed marketing campaigns for new product launches",
-        Contact: "Jane Smith (jane.smith@xyzcorp.com)",
-        Image:"",
-        },
-        
-        {
-        ID: 3,
-        Title: "Sales Associate",
-        Date: dateString,
-        Company: "123 Enterprises",
-        Location:"",
-        Position: "Part-time",
-        Description: "Assisted customers with purchases and maintained store inventory",
-        Contact: "Mark Johnson (mark.johnson@123enterprises.com)",
-        Image:"",
-        },
-        
-        {
-        ID: 4,
-        Title: "Human Resources Manager",
-        Date: dateString,
-        Company: "Acme Corporation",
-        Location:"",
-        Position: "Full-time",
-        Description: "Managed employee relations, benefits, and performance evaluations",
-        Contact: "Sara Lee (sara.lee@acmecorp.com)",
-        Image:"",
-        },
-        
-        {
-        ID: 5,
-        Title: "Graphic Designer",
-        Date: dateString,
-        Company: "Design Co.",
-        Location:"",
-        Position: "Freelance",
-        Description: "Created designs for various marketing and advertising materials",
-        Contact: "Mike Brown (mike.brown@designco.com)",
-        Image:"",
-        },
-  ];
-
-//PLEASE REFER CODESAND BOX, ANOTHE COMPONETN NEEDS TO BE MADE. which will render everything
-
+let JobCard = (props) => {
   return (
     <>
-    <Container>
+      <Container>
         <Row>
-            <Col><img src="" alt=""/></Col>
-            <Col><h3></h3></Col>
+          <Col>
+            <img src={props.Image} alt="pic" />
+          </Col>
+          <Col>
+            <h3>{props.Title}</h3>
+            <h4>{props.Company}</h4>
+            <h4>{props.Location}</h4>
+          </Col>
         </Row>
-    </Container>
+      </Container>
     </>
-  )
+  );
 };
 
 export default JobCard;
