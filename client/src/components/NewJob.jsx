ca6e88c2cc4aa3b084d75498c59e23d354fc3a47
Abis <abisali@hotmail.com>
2023-04-16 21:29:28 +0400

    Create NewJob.jsx

---
+++ client/src/components/NewJob.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import axios from "axios";
+
+//Styling
+import "../App.css";
+
+
+let Post = () =>{
+
+};
+
+export default Post;
